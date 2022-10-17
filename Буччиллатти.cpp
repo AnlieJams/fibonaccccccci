dc928ca6358ce2eac8519c24ecfb7db1d3b04eaf
AnlieJams <98762736+AnlieJams@users.noreply.github.com>
2022-10-17 14:51:29 +0300

    Add files via upload

---
+++ Буччиллатти.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+int fibonacci(int number)
+{
+    if (number == 0)
+    {
+        return 0;
+    }
+    else if (number == 1)
+    {
+        return 1;
+    }
+
+
+    return fibonacci(number - 1) + fibonacci(number - 2);
+}
+
+
+int main()
+{
+    setlocale(0, "");
+    cout << "Введите число заклинаний магии фибоначчи" << endl;
+    int number;
+
+    cin >> number;
+
+
+    cout << fibonacci(number);
+}
